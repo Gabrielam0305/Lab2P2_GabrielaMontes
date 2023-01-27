e80c3750af1b3d99b08c1afe5a3f089a3ead10e2
Gabrielam0305 <gabrielamontes0305@gmail.com>
2023-01-27 14:21:24 -0600

    creacion del ArrayList y de los metodos de creacion

--- src/lab2p2__gabrielamontes/Lab2P2__GabrielaMontes.java
+++ src/lab2p2__gabrielamontes/Lab2P2__GabrielaMontes.java
@@ -4,6 +4,7 @@
  */
 package lab2p2__gabrielamontes;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -20,8 +21,9 @@ public class Lab2P2__GabrielaMontes {
     }
 
     static void menu() {
+        ArrayList objetos = new ArrayList();
         Scanner entrada = new Scanner(System.in);
-        System.out.println("1. Registro de Inmueble/solar\n"
+        System.out.println("1. Registro de Inmueble/Solar\n"
                 + "2. Manejo de Estados \n"
                 + "3. Log in/Sign up\n"
                 + "4. Salir");
@@ -40,7 +42,7 @@ public class Lab2P2__GabrielaMontes {
         //menu
     }
     
-    static void registro (){
+    static void registro (ArrayList objetos){
         Scanner entrada = new Scanner(System.in);
         System.out.println("1. Crear,\n"
                 + " 2Listar, \n"
@@ -52,6 +54,8 @@ public class Lab2P2__GabrielaMontes {
         while (registro != 6) {
             switch (registro) {
                 case 1:
+                    //crear
+                    crear();
                     break;
                 case 2:
                     break;
@@ -63,8 +67,75 @@ public class Lab2P2__GabrielaMontes {
                     break;
                 
             }
+            //fin while
+           System.out.println("1. Crear,\n"
+                + " 2Listar, \n"
+                + "3. Modificar \n"
+                + "4. Borrar \n"
+                + "5. Vender"
+                + "6. Salir");
+         registro = entrada.nextInt();  
+            
     }
 //registro
 }
+   public static void crear(ArrayList objetos){
+       Scanner entrada = new Scanner(System.in);
+       System.out.println("1.Casa \n 2. Edificio \n 3. Solar \n 4. Salir");
+      int crear = entrada.nextInt();
+        while (crear != 4) {
+            switch (crear) {
+                case 1:
+                    //casa
+                    break;
+                case 2:
+                    //edificio
+                    break;
+                case 3:
+                    //solar
+                    break;
+            }
+            //fin while
+     System.out.println("1.Casa \n 2. Edificio \n 3. Solar \n 4. Salir");
+      crear = entrada.nextInt();
+        }
+  
+   }
+
+public static void casa(ArrayList objetos){
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("Ingrese el numero de casa: ");
+    int num=entrada.nextInt();
+    System.out.println("Ingrese el numero de bloque: ");
+    int bloc=entrada.nextInt();
+    System.out.println("Ingrese el color: ");
+    System.out.println("Ingrese el numero de baños: ");
+    int baños=entrada.nextInt();
+    System.out.println("Ingrese el ancho: ");
+    int ancho=entrada.nextInt();
+    System.out.println("Ingrese el largo: ");
+    int largo=entrada.nextInt();
+    System.out.println("Ingrese el numero de cuartos: ");
+    int cuartos=entrada.nextInt();
+}
+public static void edificio(ArrayList objetos){
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("Ingrese el numero de pisos: ");
+    int num=entrada.nextInt();
+    System.out.println("Ingrese la cantidad de locales: ");
+    int locales=entrada.nextInt();
+    System.out.println("Ingrese la dirección por referencia: ");
+    String direccion =entrada.nextLine();
+}
+public static void solar(ArrayList objetos){
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("Ingrese el ancho: ");
+    int ancho=entrada.nextInt();
+    System.out.println("Ingrese el largo: ");
+    int largo =entrada.nextInt();
+    int area=(largo*ancho);
     
+}
+
+//clase
 }
